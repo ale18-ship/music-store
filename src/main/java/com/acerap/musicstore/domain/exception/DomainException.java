6f86c2d3df195f0dd424f2279450d2aa8c825cf2
Alexis <acerap@unicartagena.edu.co>
2025-07-10 17:19:31 -0500

    Delete src directory

--- src/main/java/com/acerap/musicstore/domain/exception/DomainException.java
+++
@@ -1,7 +0,0 @@
-package com.acerap.musicstore.domain.exception;
-
-public class DomainException extends RuntimeException {
-    public DomainException(String message) {
-        super(message);
-    }
-}
